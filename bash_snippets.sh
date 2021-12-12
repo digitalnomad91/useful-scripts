caa1ceaa65399cf0a12f9b3c353fd8bb75c5023e
Andrew Corbin <andrew.c.corbin@gmail.com>
2021-12-12 14:29:00 -0600

    Update bash_snippets.sh

--- bash_snippets.sh
+++ bash_snippets.sh
@@ -8,3 +8,10 @@ alias ports='sudo lsof -i -P -n | grep LISTEN'
 
 #shortcut for searching for text in file contents
 alias fineInFile='grep -liR' #e.g. findInFile "text" file
+
+#update php & composer active CLI version
+sudo update-alternatives --set php /usr/bin/php8.0
+sudo update-alternatives --set phar /usr/bin/phar8.0
+sudo update-alternatives --set phar.phar /usr/bin/phar.phar8.0
+sudo update-alternatives --set phpize /usr/bin/phpize8.0
+sudo update-alternatives --set php-config /usr/bin/php-config8.0
