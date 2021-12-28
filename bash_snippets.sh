38ab2a1848633ab336d02d66f857ab5052322de8
Andrew Corbin <andrew.c.corbin@gmail.com>
2021-12-27 19:20:12 -0600

    updated ports alias to use netstat cmd

--- bash_snippets.sh
+++ bash_snippets.sh
@@ -4,7 +4,8 @@
 if timeout 2 bash -c "</dev/tcp/subtlefu.ge/1337"; then echo "active"; else tmux send-keys -t dangler_bot_tcp:0 "cd /root/matrix-js-sdk-bot-template && node tcpserver.js" ENTER; fi
 
 # alias for finding which process a particular port is running from
-alias ports='sudo lsof -i -P -n | grep LISTEN'
+#alias ports='sudo lsof -i -P -n | grep LISTEN'
+alias ports='netstat -tulpn | grep :'
 
 #shortcut for searching for text in file contents
 alias fineInFile='grep -liR' #e.g. findInFile "text" file
