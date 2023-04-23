b2bb3e5eb886b7caf74f988322896f0cf6d74dbc
Andrew Corbin <andrew.c.corbin@gmail.com>
2023-04-23 09:40:27 -0500

    Update docker_snippets.sh

--- docker_snippets.sh
+++ docker_snippets.sh
@@ -1,2 +1,11 @@
+
+
+#List all ports used by containers
+docker container ls --format "table {{.ID}}\t{{.Names}}\t{{.Ports}}" -a
+
+# Import mysql db in docker container
 docker exec <docker_image_id> /usr/bin/mysqldump -uroot --password=<password> <database> > filename.sql
 
+
+# Test redis connection from within docker container context.
+docker run --rm redis redis-cli -h 162.55.243.109 -p 3788
